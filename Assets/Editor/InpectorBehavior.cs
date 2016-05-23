b59bc63260cac34d5fc47f1682be66dd3d343922
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2016-05-23 17:49:36 -0500

    Editor Scripts
    Working on a editor script to minimize Inspector clutter. Got base UI
    layout working on trying to hide information.

---
+++ Assets/Editor/InpectorBehavior.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor;
+
+[CustomEditor(typeof(MinimizeInpector))]
+class InpectorBehavior : Editor
+{
+    List<Component> AllTheComponents = new List<Component>();
+    List<bool> States = new List<bool>();
+    bool GetInfo = true;
+
+    MinimizeInpector Script;
+
+    void OnEnable()
+    {
+        Script = (MinimizeInpector)target;
+
+        foreach (Component c in Script.GetComponents<Component>())
+        {
+            if (c.GetType() != typeof(MinimizeInpector) && !AllTheComponents.Contains(c))
+            {
+                AllTheComponents.Add(c);
+                States.Add(false);
+            }
+        }
+
+
+    }
+
+
+    public override void OnInspectorGUI()
+    {
+        for (int i = 0; i < States.Count; i++)
+        {
+            States[i] = GUILayout.Toggle(States[i], AllTheComponents[i].GetType().ToString());
+        }
+
+        foreach (bool b in States)
+        {
+            if (b == true)
+                AllTheComponents[States.IndexOf(b)].hideFlags = HideFlags.HideInInspector;
+            if (b == false)
+                AllTheComponents[States.IndexOf(b)].hideFlags = HideFlags.None;
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/MinimizeInpector.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class MinimizeInpector : MonoBehaviour
+{
+    public List<bool> Testing = new List<bool>();
+    public List<Component> Comp = new List<Component>();
+
+
+}
